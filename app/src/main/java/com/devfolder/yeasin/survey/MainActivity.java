5b17be9857d9f945ccb074d300c89a449f415f3e
yeasinmahi <yeasinmahi72@yahoo.com>
2019-02-05 23:06:01 +0600

    Initial

---
+++ app/src/main/java/com/devfolder/yeasin/survey/MainActivity.java
@@ -0,0 +1,167 @@
+package com.devfolder.yeasin.survey;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private AdView adView;
+    private AdRequest adRequest;
+    private InterstitialAd interstitial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CreateWebViewClient();
+        MobileAds.initialize(this, getString(R.string.app_id));
+        loadBannerAd();
+    }
+
+    public void loadBannerAd() {
+        adView = findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    public void loadInterstialAd() {
+        interstitial = new InterstitialAd(MainActivity.this);
+        interstitial.setAdUnitId(getString(R.string.interstitial_id));
+        AdRequest adRequest = new AdRequest.Builder().build();
+        interstitial.loadAd(adRequest);
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                displayInterstitial();
+            }
+        });
+    }
+
+
+    public void displayInterstitial() {
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean isConnected() {
+
+        ConnectivityManager cm = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (null != cm) {
+            NetworkInfo info = cm.getActiveNetworkInfo();
+            return (info != null && info.isConnected());
+        }
+
+        return false;
+
+    }
+
+    public void CreateWebViewClient() {
+        WebViewClient mWebClient = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (isConnected()) {
+                    view.loadUrl(url);
+                    if (url.contains("http://shatpach.com/Items/CartSummary")) {
+                        loadInterstialAd();
+                    }
+                } else {
+                    buildDialog(MainActivity.this).show();
+                }
+                return true;
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+
+            }
+        };
+        //start checking internet connection
+        if (!isConnected(this)) {
+            buildDialog(MainActivity.this).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Welcome To survey!!", Toast.LENGTH_SHORT).show();
+            // setContentView(R.layout.activity_main);
+            webView = (WebView) findViewById(R.id.webView);
+
+
+            webView.setWebViewClient(mWebClient);
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.loadUrl("http://shatpach.com");
+        }
+        //end checking internet connection
+    }
+
+
+    //start internet connection dialogue
+    public static boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public AlertDialog.Builder buildDialog(Context c) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("You need to have Mobile Data or WiFi to access this app.");
+
+        builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                CreateWebViewClient();
+            }
+
+        });
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                CreateWebViewClient();
+            }
+        });
+        return builder;
+    }
+    //end internet connection dialogue
+}
